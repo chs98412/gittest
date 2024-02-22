ea8dca028e6ee9ff06136048a3148208066d1af6
chs98412 <chs98412@gmail.com>
2024-02-22 22:34:17 +0900

    게터 추가

--- src/main/java/com/example/gittest/Member/Member.java
+++ src/main/java/com/example/gittest/Member/Member.java
@@ -1,8 +1,10 @@
 package com.example.gittest.Member;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class Member {
     private Long id;
     private String name;
